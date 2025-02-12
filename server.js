2fdc9c98f2358d02a37fc80c30e42337bfe4a07b
textencoder <160430049+textencoder@users.noreply.github.com>
2025-02-11 21:53:02 -0600

    rewrote router with express

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//node code
+/* 
 const http = require('http');
 const fs = require('fs');
 
@@ -35,3 +37,47 @@ const server = http.createServer((req, res) => {
 server.listen(8000, () => {
     console.log('now listening on 8k')
 })
+*/
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get("/", (req, res) => {
+    res.send(
+        `<h1>Welcome!</h1>
+    <p>This is the homepage.</p>
+    <a href="/about">about</a>`
+    )
+});
+
+app.get("/about", (req, res) => {
+    res.send(
+        `<h1>About</h1>
+    <p>I hail from the land down under.</p>
+    <a href="/contact-me">contact</a>`
+    )
+});
+
+app.get("/contact-me", (req, res) => {
+    res.send(
+        `<h1>Contact Me</h1>
+    <p>Let's talk about sandwiches</p>
+    <a href="/404">into the unknown</a>`
+    )
+});
+
+app.get("*", (req, res) => {
+    res.send(
+        `<h1>404 Error</h1>
+    <p>Page not found.</p>
+    <a href="/">back to home</a>`
+    )
+});
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`My second Express app - listening on port ${PORT}!`);
+});
